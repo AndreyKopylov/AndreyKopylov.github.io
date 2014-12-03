07e7e4a81cd37eb9aed34ae16e04bbec4475189c
Andrey Kopylov <andreykopylove@gmail.com>
2014-12-03 08:26:42 +0300

    update habitat

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -1,16 +1,16 @@
-var sliderValue = [1, 40, 99, 200];
-
-$(document).ready(function() {
-    $( "#slider" ).slider({
-        value: 99,
-        min: 1,
-        max: 200,
-        step: 1,
-        animate: true,
-        slide: function( event, ui ) {
-            if( sliderValue.indexOf(ui.value)===-1 ) {
-                return false;
-            }
-        }
+$(document).ready(function(){
+    $('.main').on('click',function(){
+        $(this).addClass('active');
+        $('.inbox').addClass('menu-active')
+        $('.content').addClass('menu-active')
+    $('.content').on('click', function(){
+        $(this).removeClass('menu-active');
+        $('.inbox').removeClass('menu-active')
+        $('.main').removeClass('active')
     });
+    });
+    $('#inbox-scroll').mCustomScrollbar({
+		axis: "y",
+		theme: "minimal"
+	});
 });
